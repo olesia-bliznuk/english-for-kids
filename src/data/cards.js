0f0d2d94af9bfab88aaa21f4882aea63d3b3e335
Olesya Blizniuk <olesyablizniuk@gmail.com>
2023-05-21 22:10:36 +0600

    feat: change links fot img

--- src/data/cards.js
+++ src/data/cards.js
@@ -4,49 +4,49 @@ export const cards = [
     {
       word: 'cry',
       translation: 'плакать',
-      image: '../data/img/cry.jpg',
+      image: './data/img/cry.jpg',
       audioSrc: 'audio/cry.mp3'
     },
     {
       word: 'dance',
       translation: 'танцевать',
-      image: '../data/img/dance.jpg',
+      image: './data/img/dance.jpg',
       audioSrc: 'audio/dance.mp3'
     },
     {
       word: 'dive',
       translation: 'нырять',
-      image: '../data/img/dive.jpg',
+      image: './data/img/dive.jpg',
       audioSrc: 'audio/dive.mp3'
     },
     {
       word: 'draw',
       translation: 'рисовать',
-      image: '../data/img/draw.jpg',
+      image: './data/img/draw.jpg',
       audioSrc: 'audio/draw.mp3'
     },
     {
       word: 'fish',
       translation: 'ловить рыбу',
-      image: '../data/img/fish.jpg',
+      image: './data/img/fish.jpg',
       audioSrc: 'audio/fish.mp3'
     },
     {
       word: 'fly',
       translation: 'летать',
-      image: '../data/img/fly.jpg',
+      image: './data/img/fly.jpg',
       audioSrc: 'audio/fly.mp3'
     },
     {
       word: 'hug',
       translation: 'обнимать',
-      image: '../data/img/hug.jpg',
+      image: './data/img/hug.jpg',
       audioSrc: 'audio/hug.mp3'
     },
     {
       word: 'jump',
       translation: 'прыгать',
-      image: '../data/img/jump.jpg',
+      image: './data/img/jump.jpg',
       audioSrc: 'audio/jump.mp3'
     }
   ],
@@ -54,49 +54,49 @@ export const cards = [
     {
       word: 'open',
       translation: 'открывать',
-      image: '../data/img/open.jpg',
+      image: './data/img/open.jpg',
       audioSrc: 'audio/open.mp3'
     },
     {
       word: 'play',
       translation: 'играть',
-      image: '../data/img/play.jpg',
+      image: './data/img/play.jpg',
       audioSrc: 'audio/play.mp3'
     },
     {
       word: 'point',
       translation: 'указывать',
-      image: '../data/img/point.jpg',
+      image: './data/img/point.jpg',
       audioSrc: 'audio/point.mp3'
     },
     {
       word: 'ride',
       translation: 'ездить',
-      image: '../data/img/ride.jpg',
+      image: './data/img/ride.jpg',
       audioSrc: 'audio/ride.mp3'
     },
     {
       word: 'run',
       translation: 'бегать',
-      image: '../data/img/run.jpg',
+      image: './data/img/run.jpg',
       audioSrc: 'audio/run.mp3'
     },
     {
       word: 'sing',
       translation: 'петь',
-      image: '../data/img/sing.jpg',
+      image: './data/img/sing.jpg',
       audioSrc: 'audio/sing.mp3'
     },
     {
       word: 'skip',
       translation: 'пропускать, прыгать',
-      image: '../data/img/skip.jpg',
+      image: './data/img/skip.jpg',
       audioSrc: 'audio/skip.mp3'
     },
     {
       word: 'swim',
       translation: 'плавать',
-      image: '../data/img/swim.jpg',
+      image: './data/img/swim.jpg',
       audioSrc: 'audio/swim.mp3'
     }
   ],
@@ -104,49 +104,49 @@ export const cards = [
     {
       word: 'cat',
       translation: 'кот',
-      image: '../data/img/cat.jpg',
+      image: './data/img/cat.jpg',
       audioSrc: 'audio/cat.mp3'
     },
     {
       word: 'chick',
       translation: 'цыплёнок',
-      image: '../data/img/chick.jpg',
+      image: './data/img/chick.jpg',
       audioSrc: 'audio/chick.mp3'
     },
     {
       word: 'chicken',
       translation: 'курица',
-      image: '../data/img/chicken.jpg',
+      image: './data/img/chicken.jpg',
       audioSrc: 'audio/chicken.mp3'
     },
     {
       word: 'dog',
       translation: 'собака',
-      image: '../data/img/dog.jpg',
+      image: './data/img/dog.jpg',
       audioSrc: 'audio/dog.mp3'
     },
     {
       word: 'horse',
       translation: 'лошадь',
-      image: '../data/img/horse.jpg',
+      image: './data/img/horse.jpg',
       audioSrc: 'audio/horse.mp3'
     },
     {
       word: 'pig',
       translation: 'свинья',
-      image: '../data/img/pig.jpg',
+      image: './data/img/pig.jpg',
       audioSrc: 'audio/pig.mp3'
     },
     {
       word: 'rabbit',
       translation: 'кролик',
-      image: '../data/img/rabbit.jpg',
+      image: './data/img/rabbit.jpg',
       audioSrc: 'audio/rabbit.mp3'
     },
     {
       word: 'sheep',
       translation: 'овца',
-      image: '../data/img/sheep.jpg',
+      image: './data/img/sheep.jpg',
       audioSrc: 'audio/sheep.mp3'
     }
   ],
@@ -154,49 +154,49 @@ export const cards = [
     {
       word: 'bird',
       translation: 'птица',
-      image: '../data/img/bird.jpg',
+      image: './data/img/bird.jpg',
       audioSrc: 'audio/bird.mp3'
     },
     {
       word: 'fish',
       translation: 'рыба',
-      image: '../data/img/fish1.jpg',
+      image: './data/img/fish1.jpg',
       audioSrc: 'audio/fish.mp3'
     },
     {
       word: 'frog',
       translation: 'жаба',
-      image: '../data/img/frog.jpg',
+      image: './data/img/frog.jpg',
       audioSrc: 'audio/frog.mp3'
     },
     {
       word: 'giraffe',
       translation: 'жирафа',
-      image: '../data/img/giraffe.jpg',
+      image: './data/img/giraffe.jpg',
       audioSrc: 'audio/giraffe.mp3'
     },
     {
       word: 'lion',
       translation: 'лев',
-      image: '../data/img/lion.jpg',
+      image: './data/img/lion.jpg',
       audioSrc: 'audio/lion.mp3'
     },
     {
       word: 'mouse',
       translation: 'мышь',
-      image: '../data/img/mouse.jpg',
+      image: './data/img/mouse.jpg',
       audioSrc: 'audio/mouse.mp3'
     },
     {
       word: 'turtle',
       translation: 'черепаха',
-      image: '../data/img/turtle.jpg',
+      image: './data/img/turtle.jpg',
       audioSrc: 'audio/turtle.mp3'
     },
     {
       word: 'dolphin',
       translation: 'дельфин',
-      image: '../data/img/dolphin.jpg',
+      image: './data/img/dolphin.jpg',
       audioSrc: 'audio/dolphin.mp3'
     }
   ],
@@ -204,49 +204,49 @@ export const cards = [
     {
       word: 'skirt',
       translation: 'юбка',
-      image: '../data/img/skirt.jpg',
+      image: './data/img/skirt.jpg',
       audioSrc: 'audio/skirt.mp3'
     },
     {
       word: 'pants',
       translation: 'брюки',
-      image: '../data/img/pants.jpg',
+      image: './data/img/pants.jpg',
       audioSrc: 'audio/pants.mp3'
     },
     {
       word: 'blouse',
       translation: 'блузка',
-      image: '../data/img/blouse.jpg',
+      image: './data/img/blouse.jpg',
       audioSrc: 'audio/blouse.mp3'
     },
     {
       word: 'dress',
       translation: 'платье',
-      image: '../data/img/dress.jpg',
+      image: './data/img/dress.jpg',
       audioSrc: 'audio/dress.mp3'
     },
     {
       word: 'boot',
       translation: 'ботинок',
-      image: '../data/img/boot.jpg',
+      image: './data/img/boot.jpg',
       audioSrc: 'audio/boot.mp3'
     },
     {
       word: 'shirt',
       translation: 'рубашка',
-      image: '../data/img/shirt.jpg',
+      image: './data/img/shirt.jpg',
       audioSrc: 'audio/shirt.mp3'
     },
     {
       word: 'coat',
       translation: 'пальто',
-      image: '../data/img/coat.jpg',
+      image: './data/img/coat.jpg',
       audioSrc: 'audio/coat.mp3'
     },
     {
       word: 'shoe',
       translation: 'туфли',
-      image: '../data/img/shoe.jpg',
+      image: './data/img/shoe.jpg',
       audioSrc: 'audio/shoe.mp3'
     }
   ],
@@ -254,49 +254,49 @@ export const cards = [
     {
       word: 'sad',
       translation: 'грустный',
-      image: '../data/img/sad.jpg',
+      image: './data/img/sad.jpg',
       audioSrc: 'audio/sad.mp3'
     },
     {
       word: 'angry',
       translation: 'сердитый',
-      image: '../data/img/angry.jpg',
+      image: './data/img/angry.jpg',
       audioSrc: 'audio/angry.mp3'
     },
     {
       word: 'happy',
       translation: 'счастливый',
-      image: '../data/img/happy.jpg',
+      image: './data/img/happy.jpg',
       audioSrc: 'audio/happy.mp3'
     },
     {
       word: 'tired',
       translation: 'уставший',
-      image: '../data/img/tired.jpg',
+      image: './data/img/tired.jpg',
       audioSrc: 'audio/tired.mp3'
     },
     {
       word: 'surprised',
       translation: 'удивлённый',
-      image: '../data/img/surprised.jpg',
+      image: './data/img/surprised.jpg',
       audioSrc: 'audio/surprised.mp3'
     },
     {
       word: 'scared',
       translation: 'испуганный',
-      image: '../data/img/scared.jpg',
+      image: './data/img/scared.jpg',
       audioSrc: 'audio/scared.mp3'
     },
     {
       word: 'smile',
       translation: 'улыбка',
-      image: '../data/img/smile.jpg',
+      image: './data/img/smile.jpg',
       audioSrc: 'audio/smile.mp3'
     },
     {
       word: 'laugh',
       translation: 'смех',
-      image: '../data/img/laugh.jpg',
+      image: './data/img/laugh.jpg',
       audioSrc: 'audio/laugh.mp3'
     }
   ],
@@ -304,49 +304,49 @@ export const cards = [
     {
       word: 'cake',
       translation: 'торт',
-      image: '../data/img/laugh.jpg',
+      image: './data/img/laugh.jpg',
       audioSrc: 'audio/laugh.mp3'
     },
     {
       word: 'egg',
       translation: 'яйцо',
-      image: '../data/img/laugh.jpg',
+      image: './data/img/laugh.jpg',
       audioSrc: 'audio/laugh.mp3'
     },
     {
       word: 'milk',
       translation: 'молоко',
-      image: '../data/img/laugh.jpg',
+      image: './data/img/laugh.jpg',
       audioSrc: 'audio/laugh.mp3'
     },
     {
       word: 'apple',
       translation: 'яблоко',
-      image: '../data/img/laugh.jpg',
+      image: './data/img/laugh.jpg',
       audioSrc: 'audio/laugh.mp3'
     },
     {
       word: 'banana',
       translation: 'банан',
-      image: '../data/img/laugh.jpg',
+      image: './data/img/laugh.jpg',
       audioSrc: 'audio/laugh.mp3'
     },
     {
       word: 'grape',
       translation: 'виноград',
-      image: '../data/img/laugh.jpg',
+      image: './data/img/laugh.jpg',
       audioSrc: 'audio/laugh.mp3'
     },
     {
       word: 'rice',
       translation: 'рис',
-      image: '../data/img/laugh.jpg',
+      image: './data/img/laugh.jpg',
       audioSrc: 'audio/laugh.mp3'
     },
     {
       word: 'butter',
       translation: 'масло',
-      image: '../data/img/laugh.jpg',
+      image: './data/img/laugh.jpg',
       audioSrc: 'audio/laugh.mp3'
     }
   ],
@@ -354,7 +354,7 @@ export const cards = [
     {
       word: 'bus',
       translation: 'смех',
-      image: '../data/img/laugh.jpg',
+      image: './data/img/laugh.jpg',
       audioSrc: 'audio/laugh.mp3'
     },
     {
@@ -366,37 +366,37 @@ export const cards = [
     {
       word: 'plane',
       translation: 'смех',
-      image: '../data/img/laugh.jpg',
+      image: './data/img/laugh.jpg',
       audioSrc: 'audio/laugh.mp3'
     },
     {
       word: 'train',
       translation: 'смех',
-      image: '../data/img/laugh.jpg',
+      image: './data/img/laugh.jpg',
       audioSrc: 'audio/laugh.mp3'
     },
     {
       word: 'truck',
       translation: 'смех',
-      image: '../data/img/laugh.jpg',
+      image: './data/img/laugh.jpg',
       audioSrc: 'audio/laugh.mp3'
     },
     {
       word: 'helicopter',
       translation: 'смех',
-      image: '../data/img/laugh.jpg',
+      image: './data/img/laugh.jpg',
       audioSrc: 'audio/laugh.mp3'
     },
     {
       word: 'bicycle',
       translation: 'смех',
-      image: '../data/img/laugh.jpg',
+      image: './data/img/laugh.jpg',
       audioSrc: 'audio/laugh.mp3'
     },
     {
       word: 'ship',
       translation: 'смех',
-      image: '../data/img/laugh.jpg',
+      image: './data/img/laugh.jpg',
       audioSrc: 'audio/laugh.mp3'
     }
 
@@ -406,8 +406,7 @@ export const cards = [
 export const modules = [
   {
       module: 'Action (set A)',
-      //image: 'action1.png'
-      image: '/img/action1.png'
+      image: './data/img/action1.png'
   },
   {
       module: 'Action (set B)',
@@ -415,26 +414,26 @@ export const modules = [
   },
   {
       module: 'Animal (set A)',
-      image: '/data/img/lion.png'
+      image: './data/img/lion.png'
   },
   {
       module: 'Animal (set B)',
-      image: '../data/img/animal2.png'
+      image: './data/img/animal2.png'
   },
   {
       module: 'Clothes',
-      image: 'pants.png'
+      image: './data/img/pants.png'
   },
   {
       module: 'Emotions',
-      image: './img/emotion.png'
+      image: './data/img/emotion.png'
   },
   {
       module: 'Food',
-      image: '../data/img/food.png'
+      image: './data/img/food.png'
   },
   {
       module: 'Transport',
-      image: '../data/img/car.png'
+      image: './data/img/car.png'
   },
 ];
